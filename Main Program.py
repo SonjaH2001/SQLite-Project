ec6dc02db978ad7342f1b7c2a479f214ab5bc0b4
SH <email@email.com>
2017-09-13 15:06:47 -0500

    updated CREATE, ADD, UPDATE functions.  not tested and I don't think they will work.

--- Main Program.py
+++ Main Program.py
@@ -14,12 +14,17 @@ import sqlite3  # import the library
 
 sqlite_file = 'Products_db.sqlite'  # name of the sqlite database file
 table_name1 = 'Produce_table1'  # name of the table to be created
-new_field = 'Produce_item'  # name of the field to be created
-
+id_column = 'first_field'  # name of the field to be created
+new_column_2 = '2nd_field'# name of the field to be created
+new_column_3 = '3rd_field'# name of the field to be created
+new_column_4 = '4th_field'# name of the field to be created
+new_column_5 = '5th_field'# name of the field to be created
+new_column_6 = '6th_field'# name of the field to be created
+field_type = 'TEXT' #column data type
+default_val = 'add content' #sets default value for new rows
 
 def main():
     show_menu()  # call the function to display options to the user
-    # main()
 
 def show_menu(): # provides the user with the options
     while True:
@@ -60,11 +65,50 @@ def show_menu(): # provides the user with the options
 def create_database():
     print("--->creating the database")  # for testing
 
+        # connecting to the database file
+    conn = sqlite3.connect(sqlite_file)
+    c = conn.cursor()
+        #creating table with 1 column
+    c.execute('CREATE TABLE {tn} ({idf} {ft})'\
+              .format(tn=table_name1, idf=id_column, ft=field_type))
+        #commit changes and close the DB file connection
+    conn.commit()
+    conn.close()
+
 def add_row():
     print("----->adding a row")  # for testing
 
+        # connecting to the database file
+    conn = sqlite3.connect(sqlite_file)
+    c = conn.cursor()
+        #add the new column
+#--->HOW TO add new_column_3, etc??????????????????
+    c.execute("ALTER TABLE {tn} ADD COLUMN '{cn}' {ft}"\
+              .format(tn=table_name1, cn=new_column_2, ft=field_type))
+        # commit changes and close the DB file connection
+    conn.commit()
+    conn.close()
+
 def update_row():
     print("----->updating a row")# for testing
+
+    # connecting to the database file
+    conn = sqlite3.connect(sqlite_file)
+    c = conn.cursor()
+    try: #inserts an ID w/specific val in a 3rd (new) column
+        c.execute("INSERT INTO {tn} ({idf}, {cn}) VALUES (123456, 'test')".\
+                  format(tn=table_name1, idf=id_column, cn=new_column_3))
+    except sqlite3.IntegrityError:
+        print("ERROR".format(id_column))
+        #tries to insert an ID (if it doesn't exist yet) w/spec val into a new column
+    c.execute("INSERT OR IGNORE INTO {tn} ({idf}, {cn}) VALUES (123456, 'test')".\
+        format(tn=table_name1, idf=id_column, cn=new_column_3))
+        #updates the newly inserted or pre-esisting entry
+    c.execute("UPDATE {tn} SET {cn}= ('Hey you guys!') WHERE {idf}=(123456)".\
+              format(tn=table_name1, cn=new_column_3, idf=id_column))
+    # commit changes and close the DB file connection
+    conn.commit()
+    conn.close()
 
 def delete_row():
     print("----->delete a row")  # for testing
