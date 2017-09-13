954913061efb75bc9a826c3c65ba1e6dd5ca5fe4
SH <email@email.com>
2017-09-13 14:07:44 -0500

    added function placeholders to menu

--- Main Program.py
+++ Main Program.py
@@ -21,33 +21,30 @@ def main():
     show_menu()  # call the function to display options to the user
     # main()
 
-
-def create_database():
-    print("--->creating the database")  # for testing
-
-
-def add_row():
-    print("----->adding a row")  # for testing
-
-
-def delete_row():
-    print("----->delete a row")  # for testing
-
-
 def show_menu():
     while True:
         print("Menu options:    ")
-        print("1: create database and table")
-        print("2: add a row of data to the table")
-        print("3: delete a row of data from the table")
+        print("1: CREATE a database and table")
+        print("2: ADD a row of data to the table")
+        print("3: UPDATE a row of data from the table")
+        print("4: DELETE a row of data from the table")
+        print("5: SHOW the data from entire table")
+        print("6: DISPLAY a single row of data")
         print("9: QUIT program")
+        print()
         user_input = input("Please enter the number of your selection: ")
         if user_input == "1":
             create_database()
         elif user_input == "2":
             add_row()
         elif user_input == "3":
+            update_row()
+        elif user_input == "4":
             delete_row()
+        elif user_input == "5":
+            show_all_rows()
+        elif user_input == "6":
+            show_single_row()
         elif user_input == "9":
             print()
             print("Thank you, goodbye")
@@ -57,6 +54,26 @@ def show_menu():
             print("please make a valid selection, jackass.")
             print()
             # show_menu() nope. It loops back up to the top, did while loop instead.
+
+
+def create_database():
+    print("--->creating the database")  # for testing
+
+def add_row():
+    print("----->adding a row")  # for testing
+
+def update_row():
+    print("----->updating a row")# for testing
+
+def delete_row():
+    print("----->delete a row")  # for testing
+
+def show_all_rows():
+    print("----->here is the entire table")# for testing
+
+def show_single_row():
+    print("----->here is the row you requested")# for testing
+
 
 
 main()
